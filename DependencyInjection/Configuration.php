85be733a16ead26d6bd86a6093ffd8bfff00eeec
Oskar Stark <oskarstark@googlemail.com>
2014-11-04 13:52:49 +0100

    added configuration builder

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -20,9 +20,42 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('exozet_grunt');
 
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
+        $rootNode
+            ->children()
+                ->arrayNode('environments')
+                    ->info('The environments where the bundle should be executed')
+                    ->isRequired()
+                    ->requiresAtLeastOneElement()
+                    ->defaultValue(array('dev'))
+                    ->prototype('scalar')->end()
+                ->end()
+                ->scalarNode('npm_binary_path')
+                    ->info('The binary path where npm is located')
+                    ->example('/usr/bin/npm')
+                    ->defaultValue('npm')
+                ->end()
+                ->scalarNode('bower_binary_path')
+                    ->info('The binary path where bower is located')
+                    ->example('/usr/bin/bower')
+                    ->defaultValue('bower')
+                ->end()
+                ->scalarNode('grunt_binary_path')
+                    ->info('The binary path where grunt is located')
+                    ->example('/usr/bin/grunt')
+                    ->defaultValue('bower')
+                ->end()
+                ->arrayNode('grunt_env_vars')
+                    ->info('Use environment vars for grunt with key/value pairs')
+                    ->example('LANG:    en_US.UTF-8 for LANG="en_US.UTF-8"')
+                    ->useAttributeAsKey('name')
+                    ->prototype('scalar')->end()
+                ->end()
+                ->scalarNode('grunt_task')
+                    ->info('The grunt task which should be executed')
+                    ->example('dev')
+                    ->defaultValue('dev')
+                ->end()
+            ->end();
 
         return $treeBuilder;
     }
